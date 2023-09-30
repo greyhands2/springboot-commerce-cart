f33a3db3d5d08dea83a1966912f130dae42f484b
greyhands2 <greyhands2@gmail.com>
2023-09-30 11:25:55 +0100

    save employee post rest controller

--- src/main/java/com/starq/commercecart/controller/EmployeeController.java
+++ src/main/java/com/starq/commercecart/controller/EmployeeController.java
@@ -1,8 +1,30 @@
 package com.starq.commercecart.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
+import com.starq.commercecart.service.EmployeeService;
+import com.starq.commercecart.model.Employee;
 
-
+@RestController
+@RequestMapping("/api/employees")
 public class EmployeeController {
-    
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService){
+        super();
+        this.employeeService = employeeService;
+    }
+
+    //build create employee rest api
+
+    //A @RequestBody annotation allows us to retrieve the request's body and automatically convert it to a java Object
+    @PostMapping()
+    public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee){
+        return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+    }
 }
